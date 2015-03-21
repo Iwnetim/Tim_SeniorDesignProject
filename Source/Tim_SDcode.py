92dc1c6c701764fda5ace6282f1539ede421dde3
Iwnetim Abate <iwnetm0801@gmail.com>
2015-03-20 21:02:02 -0500

    make the two changes of variable: After normalizing the potential, charge and length
    V’ ≡ V/V_T   (so V in 2.16 is equal to V’ × V_T
    x’ ≡  x/L_D  (be careful when you transform the derivative…use the chain rule).
    
    As a result, the following changes were found compared to the equations used in the previous versions of the code
    
    + ε will have cancelled out.
    + All factors of n_i (*except* in C/n_i) will have cancelled out.
    + All other factors with units (T, q, k_B) will also cancel out.
    
    In addition, C was divided twice in the pervious version instead of once but it is corrected in this version

--- Source/Tim_SDcode.py
+++ Source/Tim_SDcode.py
@@ -28,9 +28,11 @@ D_n_1=0.001*(u.m*u.m/u.s) # Diffussion cofficient for holes
 N_A=5e+23*(u.m**-3) #Charge density for holes 
 N_D=2e+24*(u.m**-3) #Charge density for holes
 
-ni= 2.1*(u.m**-3)
+ni= 2.1e12*(u.m**-3)
+
+
 Vt= k_b*T/q # Thermal Voltage to be used for potential normilzation 
-ENc=4.37 * 1017
+
 
 #C=15e+24*(u.m**-3) # change this value for charge density of donors and acceptors  
 
@@ -39,37 +41,28 @@ ENc=4.37 * 1017
 # If N_A < N_D then n= N_D-N_A. That is the semiconducotr material is n-type and  n=ni^2/p
 # If undoped n=p=ni, where ni is the electron and hole concentration when undoped
 
-#system size and division scale 
-#scaling parameters 
-#From "Deplation width(2)"
+#Build up potential, deplation width, Debye leng
  
-Vbi = k_b*T*log((N_A*N_D)/(ni*ni))                              # Barrier potential, an intially established potential difference due to the electric field formed 
+Vbi = k_b*T*log((N_A*N_D)/(ni*ni))# Barrier potential, an intially established potential difference due to the electric field formed 
+VbiT= log((N_A*N_D)/(ni*ni)) # after normalization of Vbi with Vt
 xno=(sqrt(2*epsGS*N_A*Vbi*u.m**-2/((q*q)*N_D*(N_A+N_D))))*u.m   #deplation widith for n in cm
 xpo=(sqrt(2*epsGS*N_D*Vbi*u.m**-2/((q*q)*N_A*(N_A+N_D))))*u.m   #deplation widith for p in cm
 W=sqrt(2*epsGS*Vbi*(N_A+N_D)*u.m**-2/((q*q)*N_D*N_A))*u.m       # W=xno+xpo  in cm
 Ldn=sqrt(epsGS*k_b*T*u.m**-2/(q*q*N_D))*u.m                     #Deybe length for donors
 Ldp=sqrt(epsGS*k_b*T*u.m**-2/(q*q*N_A))*u.m                    #Deybe length for acceptors
 Ldi=sqrt(epsGS*Vt*u.m**-2/(q*ni))*u.m                          #Deybe length for internsinic carrier concentration (which means when it is undoped)
-
-
+WL=sqrt(2*VbiT*ni*((1/N_D)+(1/N_A)))    # after normalization of W with Ldi
 
 # if Vt is used it is just q if KbT is used it is q^2
 #outside -xpo<x<0 it is p region and 0<xno is the n region. Outside this region the is netural due to diffusion 
-
-#localizing the deplation region: setting the domain    
-x=xpo+xno
-    
-#Using Ldi to scale the grid
-N= 20
  
-dx = x/N;
-dx=dx/Ldi    #Since meshe length is limited by Debye length; it should be normalized 
-
-
+x=WL #Since meshe length is limited by Debye length; it should be normalized 
+N= 20
+dx = x/(N-1);
 h=dx
 h2=dx*dx 
-C=np.zeros(N)
 
+C=np.zeros(N)
 for i in range (1,N):
     if i<= 10:
         C[i] = - N_A/ni
@@ -82,12 +75,14 @@ Vo=np.zeros(N)
 n=np.zeros(N)
 p=np.zeros(N)
 
-n[0]=(ni**2)/(N_A*u.m**-3)
+#charges are normlized by ni
+n[0]=(ni)/(N_A)
 p[0]=N_A*u.m**3
-p[N-1]=(ni**2)/(N_D*u.m**-3)
+p[N-1]=(ni)/(N_D)
 n[N-1]=N_D*u.m**3
 Vo[0]=0
-Vo[N-1]=Vbi*u.m**-1*u.N**-1
+Vo[N-1]=VbiT
+
 
 
 #Finding the solution
@@ -107,13 +102,16 @@ c[N-1] = 0
 b[N-1] = 1
 f[N-1] = Vo[N-1]
 
+print Vo
+print Vbi
+
 Tol=1e-5
 
 
 a=a+1/h2
 b=-2/h2+(np.exp(Vo)+np.exp(-Vo))
 c=c+1/h2
-f=-np.exp(Vo)+np.exp(-Vo)-(np.exp(Vo)-np.exp(-Vo))*Vo + C*(u.m**-3)/ni
+f=-np.exp(Vo)+np.exp(-Vo)-(np.exp(Vo)-np.exp(-Vo))*Vo + C
 print "a"
 print a
 print "b"
@@ -123,7 +121,7 @@ print c
 print "f"
 print f
  
-  
+
 #LU decompostion and iteration for possion
 #step 1, page 31,manuel-equality of L and U
 
@@ -141,6 +139,10 @@ while not taw==1:
     
     diagonals=[np.ones(N),bt[1:]]
     L=sparse.diags(diagonals,[0,-1])
+    taw=1
+    
+    print bt
+   
               
 #step 2.1,page 31,solving for Lg=f
     g=np.zeros(N)
@@ -149,10 +151,9 @@ while not taw==1:
         g[k]=f[k]-bt[k]*g[k-1] 
     diagonalsg=[np.ones(N),Vo[1:]]
     Lg=sparse.diags(diagonalsg,[0,-1])
-    print g
+    print g  
     
-    
-          
+         
 #step 2.2,page 31,solving for U*Vo=g; from n-1,n-2...2,1
     
     delta=np.zeros(N)
@@ -167,8 +168,8 @@ while not taw==1:
         delta[i]=last-Vo[i]
         Vo[i]=las
    
-    print delta
     
+   
     diagonals1=[np.ones(N),Vo[1:]]
     L=sparse.diags(diagonals1,[0,-1])
     
@@ -186,15 +187,17 @@ while not taw==1:
     if delta_max < Tol:
         taw = 1
     else:
-        for i in range (1,N-1):
-            a[i]=1/h2
-            b[i]=-((2/h2)+(exp(Vo[i])+exp(-Vo[i])))
-            c[i]=1/h2
-            f[i]=-exp(Vo[i])+exp(-Vo[i]) -((exp(Vo[i])-exp(-Vo[i]))*Vo[i])+ (C[i]*(u.m**-3))/ni
-
+        a=a+1/h2
+        b=-2/h2+(np.exp(Vo)+np.exp(-Vo))
+        c=c+1/h2
+        f=-np.exp(Vo)+np.exp(-Vo)-(np.exp(Vo)-np.exp(-Vo))*Vo + C
     
              
-print Vo    
+print Vo   
+
+
+
+
 
 
 
