a133aba1b38e1e753793d49d4664b543e41fc503
Iwnetim Abate <iwnetm0801@gmail.com>
2015-04-10 05:09:33 -0500

    Finally worked :)
    Problem with normalizing the potential at the ohmic contact was identified and  fixed
    Vo[N-1]=log((1/2)+sqrt(1+(1/4)))
    Vo[0]=log(-(N_A/(2*N_D))+sqrt((((N_A/(2*N_D))**2))+((N_D/N_D)**2)))# Potential at ohmic contact between depltion and p-region, after normailzed by Vt
    
    Also, the indexes in the while loop were rechecked to make sure that the BC's are not overwritten

--- Source/TimSD6_1_GaAs.py
+++ Source/TimSD6_1_GaAs.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from numpy import arange, pi,array, zeros
 from pylab import plot, savefig, legend
 import numpy as np
@@ -5,7 +6,6 @@ from astropy.constants import e,eps0,h,m_e
 from math import log, sqrt, exp
 from astropy import units as u
 from scipy import sparse
-
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -42,8 +42,6 @@ VbiT= log((N_A*N_D)/(ni*ni)) # after normalization of Vbi with Vt
 
 print VbiT
 print Vbi
-
-
 #deplation width
 xno=sqrt((2*epsGS*N_A*VbiT*u.N/(u.m*u.C))/(q*N_D*(N_A+N_D)))  #deplation widith for n in m
 
@@ -77,46 +75,63 @@ print h2
 
 C=np.zeros(N) # Doping concentration is different for p and  n region,
 #charges are normlized by N_D
-for i in range (1,N):
+
+Vo=np.zeros(N)
+for i in range (0,N):
     if i<= (N/2):
         C[i] = - N_A/N_D
-    elif i>(N/2): 
+
+    else: 
         C[i] = N_D/N_D
+
+    
 Vo=np.zeros(N)
 n=np.zeros(N)
 p=np.zeros(N) 
-y=np.zeros(N)  
-y[0]=0
-for i in range (1,N):
-    y[i]=y[i-1]+h
- 
-fig = plt.figure()
 
-plt.plot(y,C,label='C')
-plt.show()
 #Boundary conditions obtained from notes by 
 
+#The BC below gives the correct value on the left end but not on the right end
+'''
+for i in range(0,N):
 
-for i in range(1,N):
-    k = 0.5 * 5
+    k = 0.5 * C[i]
     if (k > 0):
-        k1 = k * (1 + sqrt(1 + 1 / (k**2)))
-    elif (k < 0):
+        k1 = k * (1 + sqrt(1 + 1 / ((k**2))))
+        
+    else:
         k1 = k * (1 - sqrt(1 + 1 / (k**2)))
-    
-    Vo[i] = log(k1)
-    n[i] = k1
-    p[i] = 1 / k1  
-    '''
-    print 'k1='+str(k1) 
-    '''    
+        Vo[i]=log(k1)
+        n[i] = k1
+        p[i] = 1 / k1
 
+        #Vo[N-1]=-log(0.5 * (N_D/N_D) * (1 + sqrt(1 + 1 / (0.5 * (N_D/N_D)**2))))
+        Vo[N-1]=0.48121
+        n[N-1]=0.5 * (N_D/N_D) * (1 + sqrt(1 + 1 / (0.5 * (-N_D/N_D)**2)))
+
+        p[N-1]=(0.5 * (-N_D/N_D) * (1 - sqrt(1 + 1 / (0.5 * (-N_D/N_D)**2))))        
+
+'''
+'''
+Vo[N-1]=0.48121
+Vo[0]=-0.124676
+'''
+
+#n=(C/(2*N_D))+sqrt(((C**2)/(4*N_D**2))+((4*N_D**2)/(4*N_D**2)) , n after normalizing by N_D, C=N_D
+#p=-((C/(2*N_D))+sqrt(((C**2)/(4*N_D**2))+((4*N_D**2)/(4*N_D**2)),p after normalizing by N_D, C=N_A
+#Vn=Vt*log(n/N_D) and Vp=Vt*log(p/N_D) derived from quasi fermi formulation
+##Vo[N-1]=log((1/2)+sqrt(1+(1/4))) # Potential at ohmic contact between depltion and n-region, after normailzed by Vt
+
+Vo[N-1]=log((1/2)+sqrt(1+(1/4)))
+Vo[0]=log(-(N_A/(2*N_D))+sqrt((((N_A/(2*N_D))**2))+((N_D/N_D)**2)))# Potential at ohmic contact between depltion and p-region, after normailzed by Vt
+
+#This is the BC writting in different way. Using this I was not able to  get the correct values on both ends
+print 'VoN-1='+str(Vo[N-1])  
+print 'Vo0='+str(Vo[0])  
 print 'Ldn='+str(Ldn)
 print 'x='+str(x)
 print 'h='+str(h)
 print 'h2='+str(h2)
-
-
 #Finding the solution
 
 #intialzing arrays 
@@ -127,24 +142,22 @@ b=np.zeros(N)
 c=np.zeros(N)
 f=np.zeros(N)
 
-a[0] = 1/h2
-c[0] = 1/h2
+a[0] = 0
+c[0] = 0
 b[0] = 1
 f[0] = Vo[0]
 a[N-1] = 0
-c[N-1] = 0
+c[N-1]=0
 b[N-1] = 1
 f[N-1] = Vo[N-1]
 
-print f
+Tol=1e-12
 
-Tol=0.00001
-
-
-a=a+1/h2
-b=-(2/h2+(np.exp(Vo)+np.exp(-Vo)))
-c=c+1/h2
-f=np.exp(Vo)-np.exp(-Vo)-C-Vo*(np.exp(Vo)+np.exp(-Vo))
+for i in range (1,N-1):
+    a[i]=1/h2
+    b[i]=-(2/h2+(exp(Vo[i])+exp(-Vo[i])))
+    c[i]=1/h2
+    f[i]=exp(Vo[i])-exp(-Vo[i])-C[i]-Vo[i]*(exp(Vo[i])+exp(-Vo[i]))
 '''
 '''
 print "a"
@@ -156,38 +169,28 @@ print c
 print "f"
 print f
 '''
+
 '''
-
-
-
-
-
-
 
 #LU decompostion and iteration for possion
 #step 1, page 31,manuel-equality of L and U
 
 taw=0  
 while not taw==1:
-    
-
     alpha[0]=b[0]
-    bt[0]=0
-    
+    bt[0]=0 
     for k in range (1,N):
         bt[k]=a[k]/alpha[k-1]
         alpha[k]=b[k]-bt[k]*c[k-1]
-  
-    
-    diagonals=[np.ones(N),bt[1:]]
-    L=sparse.diags(diagonals,[0,-1])
-
     print bt          
 #step 2.1,page 31,solving for Lg=f
     g=np.zeros(N)
     g[0]=f[0]
+    g[N-1]=f[N-1]-bt[N-1]*g[N-1]
+    #g[N-1]=f[N-1]
+    #g[N-1]=Vo[N-1]*alpha[N-1] 
     
-    for k in range (1,N):
+    for k in range (1,N-1):
         g[k]=f[k]-bt[k]*g[k-1] 
 
     print 'f'
@@ -200,49 +203,49 @@ while not taw==1:
  
     delta=np.zeros(N)
     
-    last=Vo[N-1]
-    Vo[N-1]=g[N-1]/alpha[N-1]
+    #last=Vo[N-1]
+    #Vo[N-1]=0.5 #g[N-1]/alpha[N-1]
+    
+    last=g[N-1]/alpha[N-1]    
     delta[N-1] = last- Vo[N-1] # differnce between
-       
+    Vo[N-1]=last 
     
     for i in range (N-2,-1,-1):
         last=(g[i]-c[i]*Vo[i+1])/alpha[i]
         delta[i]=last-Vo[i]
         Vo[i]=last
-    
+        
     #Finding the maximum delta 
     
     delta_max = 0
-    
-    delta_max = np.abs(delta).max()
-       
-    
-    print 'Vo'             
-    print Vo      
+    delta_max = np.abs(delta).max()   
+         
     if delta_max < Tol:
         taw = 1
-    else:
-        
-        b=-(2/h2+(np.exp(Vo)+np.exp(-Vo)))
-        f=np.exp(Vo)-np.exp(-Vo)-C-Vo*(np.exp(Vo)+np.exp(-Vo))
-    
-print 'Vo'             
-print Vo
 
+    else:
+        for i in range (1,N-1):
+            b[i]=-(2/h2+(exp(Vo[i])+exp(-Vo[i])))
+        
+            f[i]=exp(Vo[i])-exp(-Vo[i])-C[i]-Vo[i]*(exp(Vo[i])+exp(-Vo[i]))
+            
+
+'''
+
+b=-(ni/N_D)*(2/h2+(np.exp(Vo)+np.exp(-Vo)))
+        
+f=np.exp(Vo)-np.exp(-Vo)-C-Vo*(np.exp(Vo)+np.exp(-Vo))
+
+'''
+           
 nf=np.zeros(N) 
 pf=np.zeros(N)
-    
 for i in range (1,N-1):
     n = np.exp(Vo)
     p = np.exp(-Vo)
    
 nf = n*N_D
 pf = p*N_D
-
-
-
-print nf
-
 y=np.zeros(N)
 
 xn=xno*u.m/Ldn
@@ -252,42 +255,41 @@ print 'xp='+str(xp)
 print 'h='+str(h)
 print 'Np='+str(xp/h)
 print 'Nn='+str(xn/h)
-
 y[0]=0
 for i in range (1,N):
     y[i]=y[i-1]+h
  
 fig = plt.figure()
-plt.plot(y,p,label='f')
 
-plt.plot(y,n,label='n')
-plt.plot(y,Vo,label='Vo')
+plt.semilogy(y,p*N_D,label='p')
+
+plt.semilogy(y,n*N_D,label='n')
+plt.xlabel('Deplation Width')
+plt.ylabel('Carrier Density')
+
+
+'''
+plt.plot(y,Vo*Vt)
+plt.xlabel('Deplation Width')
+plt.ylabel('Potential')
+'''
+
+
+'''
 plt.plot(y,C,label='C')
+plt.xlabel('Deplation Width')
+plt.ylabel('Doping Concentration')
+'''
+
 legend()
 
-
-
 fig.suptitle('2000')
-
-'''
-'''
-plt.xlabel('xlabel')
-plt.ylabel('ylabel')
-fig.savefig('test.jpg')
-'''
-'''
-
-
 plt.show()
 
 
 
-
-
-
-
-
-        
+print Vo
+     
 
 
 
